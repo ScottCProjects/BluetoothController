01930ea1fada784737715588b1e5de114f7f1b62
Scott Camarena <scott.camarena@gm.com>
2016-02-22 18:33:05 +0000

    Initial Commit

---
+++ app/src/main/java/gmgearsforgeeks/bluetoothcontroller/BTConnectActivity.java
@@ -0,0 +1,13 @@
+package gmgearsforgeeks.bluetoothcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BTConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btconnect);
+    }
+}
